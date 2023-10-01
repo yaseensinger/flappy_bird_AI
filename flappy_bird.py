086bfed985de51be27f60433df828ec6752b0cb3
yaseensinger <yaseensinger@hotmail.com>
2023-10-01 04:23:11 +0100

    added score

--- flappy_bird.py
+++ flappy_bird.py
@@ -2,15 +2,17 @@ import pygame
 import neat
 import os
 import random 
-
+pygame.font.init()
 
 win_width = 500
 win_heingt = 800
 
-bird_img = [pygame.image.load(os.path.join("imgs","flat.png")),pygame.image.load(os.path.join("imgs","up.png")),pygame.image.load(os.path.join("imgs","down.png"))]
-lamp_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","lamp.png")))
-base_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","base.jpg")))
-bg_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bg.png")))
+bird_img = [pygame.image.load(os.path.join("imgs", "flat.png")), pygame.image.load(os.path.join("imgs", "up.png")), pygame.image.load(os.path.join("imgs", "down.png"))]
+lamp_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "lamp.png")))
+base_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.jpg")))
+bg_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+
+stat_font = pygame.font.SysFont("comicsans",40)
 
 class Bird:
     imgs =bird_img
@@ -19,7 +21,7 @@ class Bird:
     animation_time = 5
 
 
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
         self.tilt = 0
@@ -44,7 +46,7 @@ class Bird:
         if d < 0:
             d-= 2
 
-        self.y =self.y +d
+        self.y = self.y +d
 
         if d< 0 or self.y< self.height + 50:
             if self.tilt<self.max_rotation:
@@ -52,7 +54,7 @@ class Bird:
         else:
             if self.tilt > -90:
                 self.tilt -= self.rot_vel
-    def draw(self,win):
+    def draw(self, win):
         self.img_count += 1
 
         if self.img_count < self.animation_time*2:
@@ -71,7 +73,7 @@ class Bird:
              self.img_count = self.animation_time*2
         
         rotate_image = pygame.transform.rotate(self.img, self.tilt)
-        new_rect = rotate_image.get_rect(center =self.img.get_rect(topleft = (self.x,self.y)).center)
+        new_rect = rotate_image.get_rect(center =self.img.get_rect(topleft = (self.x, self.y)).center)
         win.blit(rotate_image, new_rect.topleft)
 
     def get_mask(self) :
@@ -83,13 +85,13 @@ class Lamp:
     
 
     def __init__(self,x):
-        self.x =x
+        self.x = x
         self.height =0
         self.gay = 100
 
-        self.top =0
+        self.top = 0
         self.bottom = 0
-        self.lamp_top =  pygame.transform.flip(lamp_img, False,True)
+        self.lamp_top =  pygame.transform.flip(lamp_img, False, True)
         self.lamp_bottom = lamp_img
 
         self.passed = False
@@ -115,13 +117,12 @@ class Lamp:
         top_offset = (self.x - bird.x, self.top - round(bird.y))
         bottom_offset =(self.x - bird.x, self.bottom - round(bird.y))
 
-        b_point = bird_mask.overlap(bottom_mask,bottom_offset)
-        t_point = bird_mask.overlap(top_mask,top_offset)
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        t_point = bird_mask.overlap(top_mask, top_offset)
 
         if t_point or b_point:
             return True
         return False
-
 
 class Base:
     vel = 5
@@ -144,27 +145,30 @@ class Base:
             self.x2 = self.x1 + self.width
 
     def draw(self,win):
-        win.blit(self.img,(self.x1, self.y))
-        win.blit(self.img,(self.x2, self.y))
+        win.blit(self.img, (self.x1, self.y))
+        win.blit(self.img, (self.x2, self.y))
 
 
-def draw_window(win, bird,base,lamps) :
-    win.blit(bg_img,(0,0))
+def draw_window(win, bird, base, lamps, score) :
+    win.blit(bg_img, (0,0))
 
     for lamp in lamps:
          lamp.draw(win)
+
+    text =stat_font.render("Score: "+ str(score),1,(255,255,255))
+    win.blit(text, (win_width - 10 - text.get_width(),10))
 
     base.draw(win)
     bird.draw(win)
     pygame.display.update()
 
 def main():
-    bird = Bird(230,350)
+    bird = Bird(230, 350)
     base = Base(730)
     lamps = [Lamp(700)]
     win = pygame.display.set_mode((win_width, win_heingt))
     clock = pygame.time.Clock()
-    score =0
+    score = 0
     run =True
     while run:
         clock.tick(30)
@@ -183,16 +187,20 @@ def main():
                     lamp.passed = True
                     add_lamp = True
             lamp.move()
+
         if add_lamp:
             score += 1
-            lamps.append(Lamp(700))
+            lamps.append(Lamp(600))
             lamp.move()
 
         for r in rem:
             lamps.remove(r)
 
+        if bird.y + bird.img.get_height() >= 730:
+            pass
+
         base.move()
-        draw_window(win, bird,base,lamps)
+        draw_window(win, bird, base, lamps,score)
           
     pygame.quit()
     quit()
